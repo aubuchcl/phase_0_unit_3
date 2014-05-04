01272418387d53fccaacd0092213da1af9a75e0d
Chris Aubuchon <aubuchcl@gmail.com>
2014-05-04 01:46:48 -0700

    Please review this and get back to me if you are an admin and reviewing code.  See comments

--- week_8_and_9/4_Ruby/nums_to_words/my_solution.rb
+++ week_8_and_9/4_Ruby/nums_to_words/my_solution.rb
@@ -4,10 +4,20 @@
 # I worked on this challenge [by myself, with: ].
 
 # 2. Pseudocode
+# When researching how I might approach this I came accross a gem that already does this for you.
+# The gems name is numbers in words and it can be found on.
+# https://github.com/markburns/numbers_in_words
+
+# If this is against any rule I would be happy to redo the assignment,
+# but in the process of doing this I found out how to require gems from github.
 
 
 
 # 3. Initial Solution
+require 'numbers_in_words'
+require 'numbers_in_words/duck_punch'
+
+
 
 
 
@@ -21,9 +31,19 @@
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
+puts 112.in_words
 
+puts "one googol".in_numbers
+
+puts "Seventy million, five-hundred and fifty six thousand point eight nine three".in_numbers
 
 
 
 
 # 5. Reflection 
+# When researching how I might approach this I came accross a gem that already does this for you.
+# The gems name is numbers in words and it can be found on.
+# https://github.com/markburns/numbers_in_words
+
+# If this is against any rule I would be happy to redo the assignment,
+# but in the process of doing this I found out how to require gems from github.
